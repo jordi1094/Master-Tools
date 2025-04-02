d98e3c08b8d9b799d8a99ff47bd231710e33f5dd
jordi1094 <63929965+jordi1094@users.noreply.github.com>
2025-04-02 21:07:56 +0200

    solve errors

--- staff/jordi-isern/project/api/logic/registerUser.js
+++ staff/jordi-isern/project/api/logic/registerUser.js
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
 import { User} from "../data/models/index.js";
 import { DuplicityError, SystemError } from "com/errors.js";
 import validate from "com/validate.js";
 import bcrypt from 'bcryptjs'
-=======
-import { User, Campaign, Character } from "../data/models/index.js";
-import { DuplicityError, SystemError } from "com/errors.js";
-import validate from "com/validate.js";
-import bcrypt from 'bcryptjs'
-import { campaign } from "../data/models/Campaign.js";
-import { Types } from "mongoose";
->>>>>>> 3bc5f4f253c90c23f22d5578cf9c875e9c35770f
-
 
 const registerUser = ( name, surname, email, username, role, password, passwordRepeat) => {
   validate.name(name, "name");
@@ -43,54 +33,11 @@ const registerUser = ( name, surname, email, username, role, password, passwordR
             email: email,
             username: username,
             role: role,
-<<<<<<< HEAD
             password: hash,
             campaigns: ['672cef33fa11f84b5c8da6a1']
           }
 
           User.create(newUser)
-=======
-            password: hash
-          }
-
-          return User.create(newUser)
-            .catch(error => {throw new SystemError(error.message)})
-            .then((user) => {
-              return Campaign.findById('672cef33fa11f84b5c8da6a1')
-                .catch(error => {throw new SystemError(error.message)})
-                .then(sampleCampaign => {
-
-                  const newCampaign = new Campaign({
-                    author: user._id,
-                    title: sampleCampaign.title,
-                    background: sampleCampaign.background,
-                    objective: sampleCampaign.objective,
-                    startLocation: sampleCampaign.startLocation,
-                    image: sampleCampaign.image
-                  })
-                  newCampaign.save()
-                  .catch(error => {throw new SystemError(error.message)})
-                  .then((campaign) =>{
-                    return Character.find({campaing:'672cef33fa11f84b5c8da6a1'})
-                      .catch(error => {throw new SystemError(error.message)})
-                      .then((sampleCharacters)=> {
-                        sampleCharacters.forEach((sampleCharacter) => {
-                          const newCharacter = new Character(sampleCharacter)
-                          newCharacter.campaing = campaign._id
-                          newCharacter.save()
-                          .catch(error => {
-                            throw new SystemError(error.message)
-                          })
-                          .then((newCharacterSaved) => {
-                            console.log(newCharacterSaved)
-                          })
-                        })
-                      })
-                      
-                  })
-                })
-             })
->>>>>>> 3bc5f4f253c90c23f22d5578cf9c875e9c35770f
         })
     })
 }
